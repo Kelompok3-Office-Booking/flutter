541aa098e3c233c5b418410c80a08b7ab7a33d9a
hendriari <hendriariwibowo21@gmail.com>
2022-11-16 22:41:56 +0700

    hex color

--- betterspace/lib/src/utils/colors.dart
+++ betterspace/lib/src/utils/colors.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:betterspace/src/utils/hex_color.dart';
 
 class MyColor {
-  static Color primaryColor = Colors.redAccent;
+  static HexColor redColor = HexColor('eb3734');
 }
