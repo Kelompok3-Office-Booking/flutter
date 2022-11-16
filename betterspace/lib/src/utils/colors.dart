cf6a98b5f0eb208a0fa80ed8d4895029e6af2200
hendriari <hendriariwibowo21@gmail.com>
2022-11-16 23:13:11 +0700

    logo & hex color

--- betterspace/lib/src/utils/colors.dart
+++ betterspace/lib/src/utils/colors.dart
@@ -1,4 +1,4 @@
-import 'package:betterspace/src/utils/hex_color.dart';
+import 'package:betterspace/src/utils/hex_color_convert.dart';
 
 class MyColor {
   static HexColor redColor = HexColor('eb3734');
