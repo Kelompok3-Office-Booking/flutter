d3509309010ee792bdf54ed1706aec50c85fda03
hendriari <hendriariwibowo21@gmail.com>
2022-11-16 21:13:55 +0700

    main

--- betterspace/lib/main.dart
+++ betterspace/lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:betterspace/src/screen/landing/splash_screen1.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const mainApp());
+  runApp(const BetterSpaceApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
 
-class mainApp extends StatelessWidget {
-  const mainApp({Key? key}) : super(key: key);
+class BetterSpaceApp extends StatelessWidget {
+  const BetterSpaceApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: SplashScreenOne(),
+    );
   }
 }
