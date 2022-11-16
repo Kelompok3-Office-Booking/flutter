ea3ba1d8a27ff24ba1e2bbaf757ed7c217408a78
abimanyuabi <19102054@ittelkom-pwt.ac.id>
2022-11-16 13:34:43 +0700

    first commit

---
+++ betterspace/android/app/src/main/kotlin/com/example/betterspace/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.betterspace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ betterspace/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const mainApp());
+}
+
+class mainApp extends StatelessWidget {
+  const mainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
