3888b828607386224fe05bc7596090f4297376e1
hendriari <hendriariwibowo21@gmail.com>
2022-11-18 01:28:01 +0700

    menu screen

---
+++ betterspace/lib/src/screen/menu/tersimpan/tersimpan_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TersimpanScreen extends StatelessWidget {
+  const TersimpanScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Tersimpan Screen'),
+    );
+  }
+}
