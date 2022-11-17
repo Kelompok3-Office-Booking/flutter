c377f752f513dd3aff0cf7e2220036bb9cd9bb34
hendriari <hendriariwibowo21@gmail.com>
2022-11-18 01:28:39 +0700

    main

--- betterspace/lib/main.dart
+++ betterspace/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:betterspace/src/screen/landing/splash_screen1.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'src/screen/landing/on_boarding_screen/on_boarding_view.dart';
+
 void main() {
   runApp(const BetterSpaceApp());
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
@@ -13,7 +15,8 @@ class BetterSpaceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashScreenOne(),
+      home: OnBoardinView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
