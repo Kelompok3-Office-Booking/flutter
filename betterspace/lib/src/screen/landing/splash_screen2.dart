3cb3b89427ee36e2c0d46fc87e84f1bdaa06c2ad
hendriari <hendriariwibowo21@gmail.com>
2022-11-19 00:24:11 +0700

    splash screen 2

--- betterspace/lib/src/screen/landing/splash_screen2.dart
+++ betterspace/lib/src/screen/landing/splash_screen2.dart
@@ -1,10 +1,29 @@
+import 'package:betterspace/src/utils/colors.dart';
+import 'package:betterspace/src/view_model/navigasi_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SplashScreenTwo extends StatelessWidget {
+class SplashScreenTwo extends StatefulWidget {
   const SplashScreenTwo({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreenTwo> createState() => _SplashScreenTwoState();
+}
+
+class _SplashScreenTwoState extends State<SplashScreenTwo> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      Provider.of<NavigasiViewModel>(context, listen: false)
+          .navigasiToOnboardingView(context);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: MyColor.whiteColor,
+    );
   }
 }
