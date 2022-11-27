3fede2f879df28631617759107af119e3024fe1d
abimanyuabi <19102054@ittelkom-pwt.ac.id>
2022-11-27 12:52:03 +0700

    commit sunday

--- betterspace/lib/src/screen/menu/transaksi/booking_history_screen.dart
+++ betterspace/lib/src/screen/menu/transaksi/booking_history_screen.dart
@@ -1,3 +1,4 @@
+import 'package:betterspace/src/utils/adapt_size.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,8 +14,29 @@ class _bookingHistoryScreenState extends State<bookingHistoryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [],
+      body: SizedBox(
+        height: AdaptSize.screenHeight,
+        width: AdaptSize.screenWidth,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back),
+                ),
+                Text(
+                  "Booking History",
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+            ListView(
+              children: [],
+            ),
+          ],
+        ),
       ),
     );
   }
