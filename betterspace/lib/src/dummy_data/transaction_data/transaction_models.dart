37624ec139471bffd6629ebd13f5651327e13b99
abimanyuabi <19102054@ittelkom-pwt.ac.id>
2022-12-05 19:25:20 +0700

    minor add

--- betterspace/lib/src/dummy_data/transaction_data/transaction_models.dart
+++ betterspace/lib/src/dummy_data/transaction_data/transaction_models.dart
@@ -1,10 +1,17 @@
 import 'package:betterspace/src/dummy_data/office_data/office_dummy_models.dart';
 import 'package:betterspace/src/dummy_data/user_data/user_models.dart';
 
+class PaymentMethod {
+  late String paymentMethodName;
+  late double paymentMethodFee;
+}
+
 class TransactionDetailModels {
   late String transactionUsedPromoId;
   late double transactionTotalPrice;
   late String transactionStatus;
+  late DateTime transactionCreatedAt;
+  late DateTime transactionValidationAt;
 }
 
 class TransactionModels {
