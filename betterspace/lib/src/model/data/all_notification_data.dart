3f22cde53e9dfe0b9a0e1bafacfd2d63c2d65ff4
hendriari <hendriariwibowo21@gmail.com>
2022-12-08 14:45:09 +0700

    revisi notification screen & data dummy notification

--- betterspace/lib/src/model/data/all_notification_data.dart
+++
@@ -1,8 +0,0 @@
-import 'package:betterspace/src/model/data/notification_info_data.dart';
-import 'package:betterspace/src/model/data/notification_transaction_data.dart';
-import 'package:betterspace/src/model/notification_model.dart';
-
-List<NotificationModel> allNotification = [
-  ...notificationInfo,
-  ...notificationTransaction,
-];
--- betterspace/lib/src/model/notification_model.dart
+++ betterspace/lib/src/model/notification_model.dart
@@ -1,10 +1,64 @@
+import 'package:betterspace/src/utils/adapt_size.dart';
+import 'package:betterspace/src/utils/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class NotificationModel {
-  String category, day, title, description;
+  String day, title, descriptionkey, description, image;
+  Icon logo;
+  Color backgroundColor;
 
   NotificationModel({
-    required this.category,
+    required this.logo,
+    required this.backgroundColor,
+    required this.image,
     required this.title,
     required this.day,
     required this.description,
+    required this.descriptionkey,
   });
+}
+
+class CommonDataNotification {
+  static Icon bookingAccepted = Icon(
+    Icons.done,
+    color: MyColor.neutral900,
+    size: AdaptSize.screenHeight * .014,
+  );
+  static Icon failed = Icon(
+    Icons.close,
+    color: MyColor.neutral900,
+    size: AdaptSize.screenHeight * .014,
+  );
+  static Icon waiting = Icon(
+    Icons.access_time,
+    color: MyColor.neutral900,
+    size: AdaptSize.screenHeight * .014,
+  );
+  static Icon verified = Icon(
+    Icons.wallet,
+    color: MyColor.neutral900,
+    size: AdaptSize.screenHeight * .014,
+  );
+  static Icon percent = Icon(
+    Icons.percent,
+    color: MyColor.neutral900,
+    size: AdaptSize.screenHeight * .014,
+  );
+  static Icon newBuilding = Icon(
+    CupertinoIcons.building_2_fill,
+    color: MyColor.neutral900,
+    size: AdaptSize.screenHeight * .014,
+  );
+
+  static Color successColor = MyColor.success300;
+  static Color pendingColor = MyColor.neutral400;
+  static Color failedColor = MyColor.danger300;
+  static Color percentColor = MyColor.secondary200;
+  static Color newBuildingColor = MyColor.primary400;
+
+  static String imagePromo1 = 'assets/image_assets/promo_image/promo1.png';
+  static String imagePromo2 = 'assets/image_assets/promo_image/promo2.png';
+  static String imagePromo3 = 'assets/image_assets/promo_image/promo3.png';
+  static String imageBuilding = 'assets/image_assets/space_image/space1.png';
 }
